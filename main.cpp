9f42f8d7d9ada089773c458cf437935f5def4493
ZerosOnes1 <119800394+ZerosOnes1@users.noreply.github.com>
2024-11-08 12:43:01 +0700

    feature_fast main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,5 @@
-double my_pow(double base, unsigned int exponent) {
-    double result = 1.0;
-    for (unsigned int i = 0; i < exponent; ++i) {
-        result *= base;
-    }
-    return result;
+double my_fast_pow(double base, unsigned int exponent) {
+    if (exponent == 0) return 1;
+    double half = my_fast_pow(base, exponent / 2);
+    return (exponent % 2 == 0) ? half * half : half * half * base;
 }

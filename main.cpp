9b154d29403aa49e6c07861ae1f3ba3bf3cfc378
ZerosOnes1 <119800394+ZerosOnes1@users.noreply.github.com>
2024-11-08 12:47:10 +0700

    bug_fix main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,8 @@
-double my_pow(double base, unsigned int exponent) {
+double my_pow(double base, int exponent) {
+    if (exponent < 0) {
+        base = 1 / base;
+        exponent = -exponent;
+    }
     double result = 1.0;
     for (unsigned int i = 0; i < exponent; ++i) {
         result *= base;
